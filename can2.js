2d271f5c180763314a3c3d588064afbf48416a4c
dipenmotka <dipen@atharvasystem.com>
2022-11-22 16:17:07 +0530

    Dynamic data,meta data

--- can2.js
+++ can2.js
@@ -26,13 +26,15 @@ knex.schema.hasTable('can2').then(function (exists) {
   if (!exists) {
     return knex.schema.createTable('can2', function (t) {
       t.increments('id').primary();
-      t.integer('wordpress_post_id', 45);
+      t.integer('wp_post_id', 45);
+      t.text('wp_post_meta_data', 500);
+      t.string('applied_for', 100);
       t.string('first_name', 45);
       t.string('last_name', 45);
       t.string('email', 45);
-      t.date('discarded_at', 6);
-      t.date('created_at', 6);
-      t.date('updated_at', 6);
+      t.datetime('discarded_at', 6);
+      t.string('created_at', 600);
+      t.datetime('updated_at', 6);
       t.string('tittle', 45);
       t.string('emails', 45);
       t.text('address', 45);
@@ -42,7 +44,9 @@ knex.schema.hasTable('can2').then(function (exists) {
       t.string('skypeid', 45);
       t.string('linkedinid', 45);
       t.string('description', 45);
+      t.string('agree_privacy_policy', 45);
       t.text('cover_letter', 45);
+      t.text('application_mails', 1000);
       t.integer('status_code', 45);
       t.integer('preferrred_contact_method_code', 45);
       t.integer('initial_communication', 45);
@@ -61,39 +65,60 @@ knex2('wp_posts').where({
   post_type: "awsm_job_application"
 }).select('ID')
   .then((postData) => {
-    // console.log("+++++++++++++++++++++++++++++++", postData)
+    // console.log("+++++++++++++++++++++", postData)
     for (var post of postData) {
-      console.log(post.ID)
+      // console.log(post.ID)
       knex2('wp_postmeta').where({
         post_id: post.ID
       }).select('*')
-        .then((data) => {
-          // console.log(data)
-          var first_name = JSON.stringify(data[3].meta_value).replace(/['":,{}1]+/g, "").split(" ", 1);
-          var last_name = JSON.stringify(data[3].meta_value).replace(/['":,{}1]+/g, "").split(" ").pop();
+        .then((row) => {
+          var result = new Map();
+          row.map(function (element) {
+            return result.set(element.meta_key, element.meta_value)
+          })
+          // console.log(row)
+          console.log(result)
 
+          var first_name = result.get('awsm_applicant_name').split(" ",1).toString();
+          var last_name = result.get('awsm_applicant_name').split(" ").pop();
           return knex('can2')
             .select()
             .where({
-              'wordpress_post_id': post.ID,
+              'wp_post_id': result.get('awsm_job_id'),
             })
             .then(function (rows, err) {
               if (rows.length === 0) {
+
                 // no matching records found
                 return knex('can2').insert({
-                  wordpress_post_id : data[0].post_id,
+                  wp_post_id: result.get('awsm_job_id'),
+                  applied_for: result.get('awsm_apply_for'),
                   first_name: first_name,
                   last_name: last_name,
-                  email: data[4].meta_value,
-                  mobilephone: data[5].meta_value,
-                  cover_letter: data[6].meta_value,
+                  email: result.get('awsm_applicant_email'),
+                  mobilephone: result.get('awsm_applicant_phone'),
+                  cover_letter: result.get('awsm_applicant_letter'),
+                  agree_privacy_policy:result.get('awsm_agree_privacy_policy'),
+                  application_mails: result.get('awsm_application_mails'),
+
+                  wp_post_meta_data: {
+                    wp_post_id: result.get('awsm_job_id'),
+                    applied_for: result.get('awsm_apply_for'),
+                    first_name: first_name,
+                    last_name: last_name,
+                    email: result.get('awsm_applicant_email'),
+                    mobilephone: result.get('awsm_applicant_phone'),
+                    cover_letter: result.get('awsm_applicant_letter'),
+                    agree_privacy_policy:result.get('awsm_agree_privacy_policy'),
+                    application_mails: result.get('awsm_application_mails'),
+                  }
                 }).then(() => { return console.log("data added") })
               } else {
                 throw err
               }
             })
-            .catch(function (ex) {
-              // you can find errors here.
+            .catch((err) => {
+              if (err) { throw err } else console.log("Duplicate data entry detected")
             })
         })
         .catch(function (err) {
