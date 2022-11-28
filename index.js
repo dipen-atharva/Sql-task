c22ea25183237630b5987ffe6b65ee89011fcfdd
dipenmotka <dipen@atharvasystem.com>
2022-11-28 19:56:31 +0530

    Update complete

--- index.js
+++ index.js
@@ -8,11 +8,12 @@ sourceKnex('wp_posts').where({
   .then((postData) => {
 
     for (let post of postData) {
+      // console.log(post)
       sourceKnex('wp_postmeta').where({
         post_id: post.ID
       }).select('*')
         .then((row) => {
-          var result = new Map();
+          let result = new Map();
           row.map(function (element) {
             return result.set(element.meta_key, element.meta_value)
           })
@@ -23,7 +24,7 @@ sourceKnex('wp_posts').where({
             })
             .then(function (rows, err) {
               if (rows.length === 0) {
-                // no matching records found
+                // no matching records foun
                 destKnex('candidates').insert({
                   wp_post_id: post.ID,
                   wp_applied_for: result.get('awsm_apply_for'),
@@ -36,51 +37,18 @@ sourceKnex('wp_posts').where({
                   wp_application_mails: result.get('awsm_application_mails'),
                   wp_created_at: post.post_date,
                   wp_updated_at: post.post_modified,
-                  wp_post_meta_data: {
+                  wp_post_meta_data: JSON.stringify({
                     wp_post_id: post.ID,
                     meta_data: Object.fromEntries(result)
-                  }
+                  })
                 }).then(() => { return console.log("data added") })
               } else {
                 throw err
               }
             })
             .catch(() => {
-              console.log("Duplicate data entry detected")
+              // console.log("Duplicate data entry detected")
             })
-
-          // UPDATE
-          // destKnex('candidates')
-          //   .select()
-          //   .where(('wp_updated_at', '>', post.post_date))
-          //   .then(function (rows, err) {
-          //     // if (rows.length === 0) {
-          //       // no matching records found
-          //       console.log(rows)
-          //       destKnex('candidates').update({
-          //         wp_post_id: post.ID,
-          //         wp_applied_for: result.get('awsm_apply_for'),
-          //         wp_first_name: result.get('awsm_applicant_name').split(" ", 1).toString(),
-          //         wp_last_name: result.get('awsm_applicant_name').split(" ").pop(),
-          //         wp_email: result.get('awsm_applicant_email'),
-          //         wp_mobilephone: result.get('awsm_applicant_phone'),
-          //         wp_cover_letter: result.get('awsm_applicant_letter'),
-          //         wp_agree_privacy_policy: result.get('awsm_agree_privacy_policy'),
-          //         wp_application_mails: result.get('awsm_application_mails'),
-          //         wp_created_at: post.post_date,
-          //         wp_updated_at: post.post_modified,
-          //         wp_post_meta_data: {
-          //           wp_post_id: post.ID,
-          //           // meta_data: Object.fromEntries(result)
-          //         }
-          //       }).then(() => { return console.log("data updated") })
-          //     // } else {
-          //     //   throw err
-          //     // }
-          //   })
-          //   .catch(() => {
-          //     console.log("Unknown Error")
-          //   })
         })
         .catch(function (err) {
           console.error(err);
@@ -90,6 +58,54 @@ sourceKnex('wp_posts').where({
   .catch(function (err) {
     console.error(err);
   })
+
+
+destKnex('candidates').max('wp_updated_at', { as: 'mxD' })
+  .then((date) => {
+    sourceKnex('wp_posts')
+      .select('ID', 'post_date', 'post_modified')
+      .where({
+        post_type: "awsm_job_application"
+      })
+      .where('post_modified', '>', date[0].mxD)
+      .then((ID) => {
+        for (let id of ID) {
+          console.log(id.ID)
+          sourceKnex('wp_postmeta')
+            .where({
+              post_id: id.ID
+            })
+            .select('*')
+            .then((row) => {
+              let result = new Map();
+              row.map(function (element) {
+                return result.set(element.meta_key, element.meta_value)
+              })
+              console.log(Object.fromEntries(result));
+              destKnex('candidates').where({ wp_post_id: id.ID }).update({
+                wp_post_id: id.ID,
+                wp_applied_for: result.get('awsm_apply_for'),
+                wp_first_name: result.get('awsm_applicant_name').split(" ", 1).toString(),
+                wp_last_name: result.get('awsm_applicant_name').split(" ").pop(),
+                wp_email: result.get('awsm_applicant_email'),
+                wp_mobilephone: result.get('awsm_applicant_phone'),
+                wp_cover_letter: result.get('awsm_applicant_letter'),
+                wp_agree_privacy_policy: result.get('awsm_agree_privacy_policy'),
+                wp_application_mails: result.get('awsm_application_mails'),
+                wp_updated_at: id.post_modified,
+                wp_post_meta_data: JSON.stringify({
+                  wp_post_id: id.ID,
+                  meta_data: Object.fromEntries(result),
+                })
+              }).then(() => { return console.log("data updated") })
+            })
+        }
+      })
+  })
+
+
+
+
 
 
 
