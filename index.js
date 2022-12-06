79d09f46a80c18da0ff74c24a5a687ad14c6a86b
dipenmotka <dipen@atharvasystem.com>
2022-12-06 17:28:37 +0530

    update query completed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const { sourceKnex, destKnex } = require('./db/knex');
-const rowLimit = 10;
+const rowLimit = 5;
+
 const insert = async () => {
 
   const mxCrtdDt = await destKnex('candidates').max('wp_created_at', { as: 'mxD' })
@@ -44,23 +45,23 @@ const insert = async () => {
                 'wp_post_id': post.ID,
               })
             if (destCandidates.length === 0) {
-              // await destKnex('candidates').insert({
-              //   wp_post_id: post.ID,
-              //   wp_applied_for: result.get('awsm_apply_for'),
-              //   wp_first_name: result.get('awsm_applicant_name').split(" ", 1).toString(),
-              //   wp_last_name: result.get('awsm_applicant_name').split(" ").pop(),
-              //   wp_email: result.get('awsm_applicant_email'),
-              //   wp_mobilephone: result.get('awsm_applicant_phone'),
-              //   wp_cover_letter: result.get('awsm_applicant_letter'),
-              //   wp_agree_privacy_policy: result.get('awsm_agree_privacy_policy'),
-              //   wp_application_mails: result.get('awsm_application_mails'),
-              //   wp_created_at: post.post_date,
-              //   wp_updated_at: post.post_modified,
-              //   wp_post_meta_data: JSON.stringify({
-              //     wp_post_id: post.ID,
-              //     meta_data: Object.fromEntries(result)
-              //   })
-              // })
+              await destKnex('candidates').insert({
+                wp_post_id: post.ID,
+                wp_applied_for: result.get('awsm_apply_for'),
+                wp_first_name: result.get('awsm_applicant_name').split(" ", 1).toString(),
+                wp_last_name: result.get('awsm_applicant_name').split(" ").pop(),
+                wp_email: result.get('awsm_applicant_email'),
+                wp_mobilephone: result.get('awsm_applicant_phone'),
+                wp_cover_letter: result.get('awsm_applicant_letter'),
+                wp_agree_privacy_policy: result.get('awsm_agree_privacy_policy'),
+                wp_application_mails: result.get('awsm_application_mails'),
+                wp_created_at: post.post_date,
+                wp_updated_at: post.post_modified,
+                wp_post_meta_data: JSON.stringify({
+                  wp_post_id: post.ID,
+                  meta_data: Object.fromEntries(result)
+                })
+              })
               console.log("+++++Data added", post.ID)
             } else {
               console.log("+++++Already added", post.ID)
@@ -74,132 +75,73 @@ const insert = async () => {
   return (Promise.all(wpPosts))
 }
 
+const update = async () => {
 
-// const update = async () => {
+  const mxUpdtDt = await destKnex('candidates').max('wp_updated_at', { as: 'mxD' })
+  console.log("---+++++++++++++++-UPDATE QUERY--+++++++++++++++--", mxUpdtDt[0].mxD)
+  let wpPosts = [];
+  let start = 0;
+  let decide = true;
 
-//   const mxUpdtDt = await destKnex('candidates').max('wp_updated_at', { as: 'mxD' })
-//   console.log("---+++++++++++++++-UPDATE QUERY--+++++++++++++++--", "---", mxUpdtDt[0].mxD)
-//   let wpPosts = [];
-//   let start = 0;
-//   let decide = true;
+  while (decide) {
+    wpPosts.push(await sourceKnex('wp_posts')
+      .select('ID', 'post_date', 'post_modified')
+      .where({ post_type: "awsm_job_application" })
+      .where('post_modified', '>', mxUpdtDt[0].mxD)
+      .limit(rowLimit)
+      .offset(start)
+      .then((data) => {
 
-//   while (decide) {
+        if (data.length !== rowLimit) {
+          console.log(data.length, "-----INCOMPLETE Update  -----");
+          decide = false;
+        }
+        return Promise.all(
+          data.map(async (id) => {
 
-//     wpPosts.push(await sourceKnex('wp_posts')
-//       .select('ID', 'post_date', 'post_modified')
-//       .where({
-//         post_type: "awsm_job_application"
-//       })
-//       .where('post_modified', '>', mxUpdtDt[0].mxD)
-//       .limit(rowLimit)
-//       .offset(start)
-//       .then((data) => {
-//         return Promise.all(
-//           data.map(async (id) => {
+            const sourcePostMeta = await sourceKnex('wp_postmeta')
+              .where({ post_id: id.ID })
+              .select('*')
 
-//             console.log("Last Update :- ", mxUpdtDt[0].mxD)
-//             console.log(id.ID)
-//             sourceKnex('wp_postmeta')
-//               .where({
-//                 post_id: id.ID
-//               })
-//               .select('*')
-//               .then((row) => {
-//                 let result = new Map();
-//                 row.map(function (element) {
-//                   return result.set(element.meta_key, element.meta_value)
-//                 })
-//                 console.log(Object.fromEntries(result));
-//                 return destKnex('candidates')
-//                   .where({ wp_post_id: id.ID })
-//                   .update({
-//                     wp_post_id: id.ID,
-//                     wp_applied_for: result.get('awsm_apply_for'),
-//                     wp_first_name: result.get('awsm_applicant_name').split(" ", 1).toString(),
-//                     wp_last_name: result.get('awsm_applicant_name').split(" ").pop(),
-//                     wp_email: result.get('awsm_applicant_email'),
-//                     wp_mobilephone: result.get('awsm_applicant_phone'),
-//                     wp_cover_letter: result.get('awsm_applicant_letter'),
-//                     wp_agree_privacy_policy: result.get('awsm_agree_privacy_policy'),
-//                     wp_application_mails: result.get('awsm_application_mails'),
-//                     wp_updated_at: id.post_modified,
-//                     wp_post_meta_data: JSON.stringify({
-//                       wp_post_id: id.ID,
-//                       meta_data: Object.fromEntries(result),
-//                     })
-//                   })
-//                 console.log("data updated")
+            let result = new Map();
+            sourcePostMeta.map(function (element) {
+              return result.set(element.meta_key, element.meta_value)
+            })
+            // console.log(id.ID)
+            // console.log(Object.fromEntries(result));
 
-//               })
-//           })
-//         )
-//       })
-//     )
-//     start += rowLimit;
-//   }
-//   return (Promise.all(wpPosts))
-// }
-
-// await destKnex('candidates').max('wp_updated_at', { as: 'mxD' })
-//   .then((date) => {
-//     console.log("----UPDATE QUERY----", totalRecords, date[0].mxD)
-//     for (let start = 0; start < totalRecords[0].ttlRec;) {
-//       // start += rowLimit;
-//       sourceKnex('wp_posts')
-//         .select('ID', 'post_date', 'post_modified')
-//         .where({
-//           post_type: "awsm_job_application"
-//         })
-//         .where('post_modified', '>', date[0].mxD)
-//         .limit(rowLimit)
-//         .offset(start)
-//         .then((ID) => {
-//           // console.log("+++++++++++++++++++++++++++++++")
-//           console.log(start, "---")
-//           for (let id of ID) {
-//             console.log("Last Update :- ", date[0].mxD)
-//             console.log(id.ID)
-//             return sourceKnex('wp_postmeta')
-//               .where({
-//                 post_id: id.ID
-//               })
-//               .select('*')
-//               .then((row) => {
-//                 let result = new Map();
-//                 row.map(function (element) {
-//                   return result.set(element.meta_key, element.meta_value)
-//                 })
-//                 console.log(Object.fromEntries(result));
-//                 return destKnex('candidates').where({ wp_post_id: id.ID }).update({
-//                   wp_post_id: id.ID,
-//                   wp_applied_for: result.get('awsm_apply_for'),
-//                   wp_first_name: result.get('awsm_applicant_name').split(" ", 1).toString(),
-//                   wp_last_name: result.get('awsm_applicant_name').split(" ").pop(),
-//                   wp_email: result.get('awsm_applicant_email'),
-//                   wp_mobilephone: result.get('awsm_applicant_phone'),
-//                   wp_cover_letter: result.get('awsm_applicant_letter'),
-//                   wp_agree_privacy_policy: result.get('awsm_agree_privacy_policy'),
-//                   wp_application_mails: result.get('awsm_application_mails'),
-//                   wp_updated_at: id.post_modified,
-//                   wp_post_meta_data: JSON.stringify({
-//                     wp_post_id: id.ID,
-//                     meta_data: Object.fromEntries(result),
-//                   })
-//                 }).then(() => {
-//                   return console.log("data updated")
-//                 })
-//               })
-//           }
-//         })
-//       start += rowLimit;
-//     }
-//   })
-
+            const updtCandidates = await destKnex('candidates')
+              .where({ wp_post_id: id.ID })
+              .update({
+                wp_post_id: id.ID,
+                wp_applied_for: result.get('awsm_apply_for'),
+                wp_first_name: result.get('awsm_applicant_name').split(" ", 1).toString(),
+                wp_last_name: result.get('awsm_applicant_name').split(" ").pop(),
+                wp_email: result.get('awsm_applicant_email'),
+                wp_mobilephone: result.get('awsm_applicant_phone'),
+                wp_cover_letter: result.get('awsm_applicant_letter'),
+                wp_agree_privacy_policy: result.get('awsm_agree_privacy_policy'),
+                wp_application_mails: result.get('awsm_application_mails'),
+                wp_updated_at: id.post_modified,
+                wp_post_meta_data: JSON.stringify({
+                  wp_post_id: id.ID,
+                  meta_data: Object.fromEntries(result),
+                })
+              })
+            console.log("data updated")
+          })
+        )
+      })
+    )
+    start += rowLimit;
+  }
+  return (Promise.all(wpPosts))
+}
 
 const run = async () => {
   try {
     await insert()
-    // await update()
+    await update()
     process.exit(0)
   } catch (err) {
     console.error(err)
